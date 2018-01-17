8c1819556b923c40f79e885e3d73fe2dac617c93
Sagnik-Adusumilli <sagnik.adusumilli@gmail.com>
2018-01-16 21:39:58 -0500

    integrated map feature into the application

--- assets/app/map/map.component.ts
+++ assets/app/map/map.component.ts
@@ -6,11 +6,24 @@ import { Component } from '@angular/core';
     styleUrls: ['map.component.css']
 })
 export class MapComponent {
- public defaultLatitude: number = 51.678418;
- public defaultLongitude: number = 7.809007;
- public latitude: number = this.defaultLatitude;
- public longitude: number = this.defaultLongitude;
- public locationChosen = false; 
+ public latitude;
+ public longitude;
+ public locationChosen = false;
+ public zoom = 9;
+ public mapCenter = [51.35459712, 0.073658347];
+ 
+ public clinicMarkers = [
+     {
+        lat: 51.517063, 
+        lng: -0.098697,
+        name: 'St. Bartholomew\'s Hospital'
+     },
+     {
+        lat: 51.131670,
+        lng: 0.277765,
+        name: 'Nuffield Health Tunbridge Wells Hospital'
+     }
+ ];
 
  public onChoseLocation(event) {
      this.latitude = event.coords.lat;
